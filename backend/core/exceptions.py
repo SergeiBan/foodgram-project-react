3becfd1ba7a636ad1714be84aecc6399166f3420
Sergei Banshchikov <s.o.banshchikov@yandex.ru>
2022-10-25 02:13:20 +0200

    Custom 404 message fixed

--- backend/core/exceptions.py
+++ backend/core/exceptions.py
@@ -2,10 +2,9 @@ from rest_framework.views import exception_handler
 
 
 def custom404_exception(exc, context):
-    views = {
-        
-    }
     response = exception_handler(exc, context)
-    print('recipes' in str(context['view']), context)
-    response.data['detail'] = 'К сожалению, запрошенная информация не найдена'
+    if 'not_found' in str(response.data):
+        response.data['detail'] = (
+            'К сожалению, запрошенная информация не найдена')
+
     return response
